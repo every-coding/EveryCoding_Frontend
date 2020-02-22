0cfeff029e4153698318b6a9965c128fdd927aa3
junhp1234 <junhp1234@cu.ac.kr>
2020-02-22 23:03:07 +0900

    Changed Info : by 박준홍 - Implement Lecture Student list page 	- create lecture student page

--- src/pages/admin/views/lecture/StudentList.vue
+++ src/pages/admin/views/lecture/StudentList.vue
@@ -1,83 +1,242 @@
-<template><!--관리자 페이지의 수강과목 생성 페이지 -->
+<template>
   <div class="view">
-    <Panel :title="title">
-      <el-form label-position="top">
+    <Panel :title="$t('m.User_User') ">
+      <div slot="header">
         <el-row :gutter="20">
-          <el-col :span="24">
-            <el-form-item :label="$t('m.LectureTitle')" required>
-              <el-input v-model="lecture.title" :placeholder="$t('m.LectureTitle')"></el-input>
-            </el-form-item>
+          <el-col :span="selectedUsers.length ? 16: 24">
+            <el-input v-model="keyword" prefix-icon="el-icon-search" placeholder="Keywords"></el-input>
           </el-col>
-          <el-col :span="24">
-            <el-form-item :label="$t('m.LectureDescription')" required>
-              <Simditor v-model="lecture.description"></Simditor>
-            </el-form-item>
-          </el-col>
-		  <el-col :spane="8">
-		  	<el-form-item :label="$t('m.LectureStatus')" required>
-			  <el-switch
-			    v-model="lecture.status"
-				active-text=""
-				inactive-text="">
-			  </el-switch>
-			</el-form-item>
-		  </el-col>
-		  <el-col :spane="8">
-		  	<el-form-item :label="$t('m.Lecture_Password')" required>
-			  <el-input
-			    v-model="lecture.password"
-				active-text=""
-				inactive-text="">
-			  </el-input>
-			</el-form-item>
-		  </el-col>
         </el-row>
-      </el-form>
-      <save @click.native="saveLecture"></save>
+      </div>
+      <el-table
+        v-loading="loadingTable"
+        element-loading-text="loading"
+        @selection-change="handleSelectionChange"
+        ref="table"
+        :data="userList"
+        style="width: 100%">
+        <el-table-column type="selection" width="55"></el-table-column>
+
+        <el-table-column prop="id" label="ID"></el-table-column>
+
+        <el-table-column prop="username" label="Username"></el-table-column>
+
+        <el-table-column prop="create_time" label="Create Time">
+          <template slot-scope="scope">
+            {{scope.row.create_time | localtime }}
+          </template>
+        </el-table-column>
+
+        <el-table-column prop="last_login" label="Last Login">
+          <template slot-scope="scope">
+            {{scope.row.last_login | localtime }}
+          </template>
+        </el-table-column>
+
+        <el-table-column prop="real_name" label="Real Name"></el-table-column>
+
+        <el-table-column prop="email" label="Email"></el-table-column>
+
+        <el-table-column prop="admin_type" label="User Type">
+          <template slot-scope="scope">
+            {{ scope.row.admin_type }}
+          </template>
+        </el-table-column>
+
+        <el-table-column fixed="right" label="Option" width="200">
+          <template slot-scope="{row}">
+            <icon-btn name="Accept" icon="edit" @click.native="AcceptStudent"></icon-btn>
+          </template>
+        </el-table-column>
+      </el-table>
+      <div class="panel-options">
+        <el-pagination
+          class="page"
+          layout="prev, pager, next"
+          @current-change="currentChange"
+          :page-size="pageSize"
+          :total="total">
+        </el-pagination>
+      </div>
     </Panel>
   </div>
 </template>
 
 <script>
+  import papa from 'papaparse'
   import api from '../../api.js'
-  import Simditor from '../../components/Simditor.vue'
+  import utils from '@/utils/utils'
 
   export default {
-    name: 'CreateLecture',
-    components: {
-      Simditor
-    },
+    name: 'User',
     data () {
       return {
-        title: 'Create Lecture',
-        disableRuleType: false,
-        lecture: {
-          title: '',
-          description: '',
-          status: false,
-          password: ''
+        lectureID: '',
+        // 一页显示的用户数
+        pageSize: 10,
+        // 用户总数
+        total: 0,
+        // 用户列表
+        userList: [],
+        uploadUsers: [],
+        uploadUsersPage: [],
+        uploadUsersCurrentPage: 1,
+        uploadUsersPageSize: 15,
+        // 搜索关键字
+        keyword: '',
+        // 是否显示用户对话框
+        showUserDialog: false,
+        // 当前用户model
+        user: {},
+        loadingTable: false,
+        loadingGenerate: false,
+        // 当前页码
+        currentPage: 0,
+        selectedUsers: [],
+        formGenerateUser: {
+          prefix: '',
+          suffix: '',
+          number_from: 0,
+          number_to: 0,
+          password_length: 8
         }
       }
     },
+    mounted () {
+      this.lectureID = this.$route.params.lectureId
+      this.getUserList(1)
+      console.log(this.lectureID)
+    },
     methods: {
-      saveLecture () {
-        let funcName = this.$route.name === 'edit-lecture' ? 'editLecture' : 'createLecture'
-        let data = Object.assign({}, this.lecture)
-        api[funcName](data).then(res => {
-          this.$router.push({name: 'lecture-list', query: {refresh: 'true'}})
+      // 切换页码回调
+      currentChange (page) {
+        this.currentPage = page
+        this.getUserList(page)
+      },
+      // 提交修改用户的信息
+      saveUser () {
+        api.editUser(this.user).then(res => {
+          // 更新列表
+          this.getUserList(this.currentPage)
+        }).then(() => {
+          this.showUserDialog = false
         }).catch(() => {
         })
+      },
+      // 打开用户对话框
+      openUserDialog (id) {
+        this.showUserDialog = true
+        api.getUser(id).then(res => {
+          this.user = res.data.data
+          this.user.password = ''
+          this.user.real_tfa = this.user.two_factor_auth
+        })
+      },
+      // 获取用户列表
+      getUserList (page) {
+        this.loadingTable = true
+        api.getLectureUserList((page - 1) * this.pageSize, this.pageSize, this.keyword, this.lectureID).then(res => {
+          this.loadingTable = false
+          this.total = res.data.data.total
+          this.userList = res.data.data.results
+        }, res => {
+          this.loadingTable = false
+        })
+      },
+      handleSelectionChange (val) {
+        this.selectedUsers = val
+      },
+      generateUser () {
+        this.$refs['formGenerateUser'].validate((valid) => {
+          if (!valid) {
+            this.$error('Please validate the error fields')
+            return
+          }
+          this.loadingGenerate = true
+          let data = Object.assign({}, this.formGenerateUser)
+          api.generateUser(data).then(res => {
+            this.loadingGenerate = false
+            let url = '/admin/generate_user?file_id=' + res.data.data.file_id
+            utils.downloadFile(url).then(() => {
+              this.$alert('All users created successfully, the users sheets have downloaded to your disk.', 'Notice')
+            })
+            this.getUserList(1)
+          }).catch(() => {
+            this.loadingGenerate = false
+          })
+        })
+      },
+      handleUsersCSV (file) {
+        papa.parse(file, {
+          complete: (results) => {
+            let data = results.data.filter(user => {
+              return user[0] && user[1] && user[2]
+            })
+            let delta = results.data.length - data.length
+            if (delta > 0) {
+              this.$warning(delta + ' users have been filtered due to empty value')
+            }
+            this.uploadUsersCurrentPage = 1
+            this.uploadUsers = data
+            this.uploadUsersPage = data.slice(0, this.uploadUsersPageSize)
+          },
+          error: (error) => {
+            this.$error(error)
+          }
+        })
+      },
+      handleUsersUpload () {
+        api.importUsers(this.uploadUsers).then(res => {
+          this.getUserList(1)
+          this.handleResetData()
+        }).catch(() => {
+        })
+      },
+      handleResetData () {
+        this.uploadUsers = []
       }
     },
-    mounted () {
-      if (this.$route.name === 'edit-lecture') {
-        this.title = 'Edit Lecture'
-        api.getLecture(this.$route.params.lectureId).then(res => {
-          let data = res.data.data
-          this.lecture = data
-        }).catch(() => {
-        })
+    computed: {
+      selectedUserIDs () {
+        let ids = []
+        for (let user of this.selectedUsers) {
+          ids.push(user.id)
+        }
+        return ids
+      }
+    },
+    watch: {
+      'keyword' () {
+        this.currentChange(1)
+      },
+      'user.admin_type' () {
+        if (this.user.admin_type === 'Super Admin') {
+          this.user.problem_permission = 'All'
+        } else if (this.user.admin_type === 'Regular User') {
+          this.user.problem_permission = 'None'
+        }
+      },
+      'uploadUsersCurrentPage' (page) {
+        this.uploadUsersPage = this.uploadUsers.slice((page - 1) * this.uploadUsersPageSize, page * this.uploadUsersPageSize)
       }
     }
   }
 </script>
+
+<style scoped lang="less">
+  .import-user-icon {
+    color: #555555;
+    margin-left: 4px;
+  }
+
+  .userPreview {
+    padding-left: 10px;
+  }
+
+  .notification {
+    p {
+      margin: 0;
+      text-align: left;
+    }
+  }
+</style>
